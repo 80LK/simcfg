4151c927da93cc5f6d7a576ed1ba23ebb9de4af8
80LK <80lkru@gmail.com>
2021-11-21 00:08:48 +0300

    add write in test

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@ import Config from "./index.js";
 
 let cfg = Config.parseFromFileSync("test.json");
 
-console.group("JSON");
+console.log("JSON");
 console.log("get a:", cfg.get("a"));
 console.log("get b.a:", cfg.get("b.a"));
 console.log("get c:", cfg.get("c"));
@@ -13,8 +13,12 @@ console.log("get c.d.a:", cfg.get("c.d.a"));
 console.log("get c.d.c:", cfg.get("c.d.c", "defaultValue"));
 console.groupEnd();
 
-cfg = Config.parseFromFileSync("test.yml");
-console.group("YAML");
+cfg.set("c.d.c", 123);
+console.log("set c.d.c, get:", cfg.get("c.d.c", "defaultValue"));
+cfg.writeFileSync("test.w.json");
+
+cfg = Config.parseFromFileSync("test.yml", true);
+console.log("YAML");
 console.log("get a:", cfg.get("a"));
 console.log("get b.a:", cfg.get("b.a"));
 console.log("get c:", cfg.get("c"));
@@ -23,4 +27,10 @@ console.log("get c, get b:", cfg.get<Config>("c").get("b"));
 console.log("get c.c.a:", cfg.get("c.c.a"));
 console.log("get c.d.a:", cfg.get("c.d.a"));
 console.log("get c.d.c:", cfg.get("c.d.c", "defaultValue"));
-console.groupEnd();
+
+try {
+	console.log("yaml immutable", cfg.getImmutable());
+	cfg.set("c.d.c", 123);
+} catch (e) {
+	console.error("Error:", e);
+}
